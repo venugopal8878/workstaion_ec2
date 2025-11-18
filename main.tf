9732bc80160270928c91f56b4ac8535c5917f365
venugopal <mallavallivenu@gmail.com>
2025-11-18 15:33:55 +0530

    akks

---
+++ main.tf
@@ -0,0 +1,39 @@
+resource "ec2_instance" "ec2" {
+       name= var.name
+       ami_id=data.ami_id
+       instance_type=var.instance_name
+       vpc_security_group_ids =[aws.security_group.venu.id]
+
+       tags = {
+
+         Name = var.name
+       }
+
+}
+
+resource "security_group" "venu"{
+    name=var.security_group_name
+    description= "aloow all"
+
+    egress {
+
+     from_port = 0
+     to_port = 0
+     protocol = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+     ipv6_cidr_blocks = ["::/0"]
+
+    }
+
+    ingress {
+        from_port= var.from_port
+        to_port = var.to_port
+        protocol= "tcp"
+        cidr_blocks =["0.0.0.0/0"]
+        ipv6_cidr_blocks =["::/0"]
+    }
+
+    tags ={
+        Name = var.name
+    }
+}
---
+++ variable.tf
@@ -0,0 +1,31 @@
+variable "name"{
+    type=string
+    default ="ec2"
+}
+
+variable "instance_name"{
+    default=var.environment == "prod" ? "t3.small": "t2.micro"
+}
+
+variable "environment"{
+    type = string
+    default ="dev"
+}
+
+variable "security_group_name" {
+    type= string
+    default = "security_group"
+  
+}
+
+variable "from_port" {
+    type =list(numbers)
+    default = [22, 80, 443]
+  
+}
+
+variable "to_port" {
+    type = list(numbers)
+    default = [22, 80, 443]
+  
+}
